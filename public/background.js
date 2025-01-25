7f0e84333b6a273b38c40f1dd5f8c217c2e4143a
James Michael <jomichael1994@gmail.com>
2025-01-25 17:53:01 +0000

    cleanup

--- public/background.js
+++ public/background.js
@@ -1,25 +1,5 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 	console.error('[background] message received.');
-	// if (message.command === 'initiateDownload') {
-	// 	const {
-	// 		options: { audioOnly, playlist, url },
-	// 	} = message;
-	// 	console.error('url', url);
-	// 	const type = audioOnly ? 'audio' : 'video';
-	// 	console.error('Command execution requested');
-	// 	fetch('http://localhost:6556/download', {
-	// 		method: 'POST',
-	// 		body: JSON.stringify({
-	// 			url,
-	// 			type,
-	// 			shouldDownloadPlaylist: playlist,
-	// 		}),
-	// 		headers: {
-	// 			'Content-Type': 'application/json',
-	// 		},
-	// 	});
-	// 	// sendResponse({ status: 'Command execution is not directly possible' });
-	// }
 	if (message.command === 'initiateDownloadV2') {
 		const {
 			options: { url },
