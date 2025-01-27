fd2a1821a4de12d6a45a0ec977d46ee3a57b24d6
James Michael <jomichael1994@gmail.com>
2025-01-27 14:18:13 +0000

    add audio and playlist toggles

--- public/background.js
+++ public/background.js
@@ -2,14 +2,16 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 	console.error('[background] message received.');
 	if (message.command === 'initiateDownloadV2') {
 		const {
-			options: { url },
+			options: { url, type, playlist },
 		} = message;
 		console.error('url', url);
 		console.error('Command execution requested');
-		fetch('http://localhost:6556/download', {
+		fetch('http://localhost:6556/jobs', {
 			method: 'POST',
 			body: JSON.stringify({
 				url,
+				type,
+				playlist,
 			}),
 			headers: {
 				'Content-Type': 'application/json',
--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 
 function App() {
 	const [currentUrl, setCurrentUrl] = useState('');
+	const [isAudioOnly, setIsAudioOnly] = useState(false);
+	const [isPlaylist, setIsPlaylist] = useState(false);
 
 	useEffect(() => {
 		window.chrome.tabs.query(
@@ -20,12 +22,17 @@ function App() {
 			command: 'initiateDownloadV2',
 			options: {
 				url: currentUrl,
+				type: isAudioOnly ? 'audio' : 'video',
+				playlist: isPlaylist,
 			},
 		};
 		window.chrome.runtime.sendMessage(message, (response) => {
 			console.log(response);
 		});
 	};
+
+	const isPlaylistAllowed =
+		/youtube\.com/.test(currentUrl) && /list=/.test(currentUrl);
 
 	return (
 		<div className="select-none p-8 h-fit w-fit bg-gradient-to-br from-neutral-800 to-neutral-950">
@@ -41,9 +48,46 @@ function App() {
 			<div className="font-copy flex flex-col gap-4 items-center">
 				<input
 					value={currentUrl}
+					title={currentUrl}
 					disabled
 					className="rounded-full cursor-not-allowed bg-neutral-700 py-3 w-60 px-3 text-gray-400"
 				></input>
+
+				<div className="grid grid-cols-2 justify-center items-center">
+					<label className="flex gap-1.5 justify-start items-center cursor-pointer">
+						<input
+							type="checkbox"
+							checked={isAudioOnly}
+							onChange={() => setIsAudioOnly((prev) => !prev)}
+							className="sr-only peer"
+						/>
+						<div className="relative w-11 h-6 bg-gray-300 peer-focus:outline-none peer-focus:ring-1 peer-focus:ring-yellow-600 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-yellow-600"></div>
+						<span className="leading-none font-copy text-xs text-gray-300">
+							Audio Only
+						</span>
+					</label>
+
+					<label
+						className={`flex gap-1.5 justify-end items-center ${
+							isPlaylistAllowed
+								? 'cursor-pointer'
+								: 'cursor-not-allowed opacity-50'
+						}`}
+					>
+						<input
+							type="checkbox"
+							checked={isPlaylist}
+							disabled={!isPlaylistAllowed}
+							onChange={() => setIsPlaylist((prev) => !prev)}
+							className="sr-only peer"
+						/>
+						<div className="relative w-11 h-6 bg-gray-300 peer-focus:outline-none peer-focus:ring-1 peer-focus:ring-yellow-600 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-yellow-600"></div>
+						<span className="leading-none font-copy text-xs text-gray-300">
+							Playlist
+						</span>
+					</label>
+				</div>
+
 				<button
 					onClick={sendMessage}
 					className="rounded bg-gradient-to-r from-blue-600 to-blue-700 px-8 py-2 text-white text-sm hover:from-blue-700 hover:to-blue-800"
